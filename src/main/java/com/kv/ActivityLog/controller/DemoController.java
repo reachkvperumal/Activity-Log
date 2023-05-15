d80ece217ea0928b20b189698762909e87cdb140
KV <indianakv@gmail.com>
2023-05-14 20:18:47 -0400

    Activity-Log init commit

---
+++ src/main/java/com/kv/ActivityLog/controller/DemoController.java
@@ -0,0 +1,37 @@
+package com.kv.ActivityLog.controller;
+
+import com.kv.ActivityLog.dto.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+
+    @GetMapping("/time")
+    public String time(@RequestParam(defaultValue = "EST") Optional<String> zoneId) {
+        String current = zoneId.get();
+        String id = Optional.ofNullable(ZoneId.SHORT_IDS.get(current.toUpperCase())).orElseGet(() -> "-05:00");
+        log.info("Passed TimeZone: {} - Looked up TimeZone: {}", current, id);
+        return LocalDateTime.now(ZoneId.of(id)).toString();
+    }
+
+    @PostMapping("/log")
+    public String log(@RequestBody Person person) {
+        log.info("First Name: {}, Last Name: {}", person.getFirstName(), person.getLastName());
+        Exception exp = null;
+        try {
+            Thread.sleep(5000);
+            Thread.currentThread().interrupt();
+        } catch (InterruptedException e) {
+            exp = e;
+        }
+        throw new RuntimeException(exp);
+    }
+}
---
+++ src/test/java/com/kv/ActivityLog/ActivityLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kv.ActivityLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
